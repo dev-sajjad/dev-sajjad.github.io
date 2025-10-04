4ac3b88b1a8c92d882cdafd24ca9bf6cb7de65b0
Sajjad Hussain <98208822+dev-sajjad@users.noreply.github.com>
2025-10-04 23:49:07 +0600

    Set Vite base path to root

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/Personal-Portfolio-Website/"
-      : "/",
+  base: '/',
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
